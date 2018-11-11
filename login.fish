b9fe0b938badf338be5beeee070e0fdb7684cce0
alxptt <alxpettit@gmail.com>
2018-11-11 00:01:18 -0500

    haaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ login.fish
@@ -0,0 +1,7 @@
+# Copyleft Alexandra Pettit 2018
+
+# Just a bit of unecessary boilerplate for later, really
+
+if test -e /etc/fish/login-override.fish
+    source /etc/fish/login-override.fish
+end
