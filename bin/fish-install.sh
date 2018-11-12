16da45d91980450100108570d415fd1c97ed17a4
alxptt <alxpettit@gmail.com>
2018-11-12 18:13:34 -0500

    normalize stuff more

---
+++ bin/fish-install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Dirt simple install script for my cute little FISH config.
+# Should I even bother copyrighting this? Sure, it's GPLv3 too. Everything is GPLv3. My ass is GPLv3.
+
+wget "https://raw.githubusercontent.com/alxpettit/fish/master/interactive.fish" -O /etc/fish/interactive.fish
+wget "https://raw.githubusercontent.com/alxpettit/fish/master/config.fish" -O /etc/fish/config.fish
+wget "https://raw.githubusercontent.com/alxpettit/fish/master/login.fish" -O /etc/fish/config.fish
